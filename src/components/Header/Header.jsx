476f405dd338c6f9a1f695b17fd833922f505466
Gil <gilvicentevieira@gmail.com>
2024-01-04 11:58:47 +0000

    Concluded custom select

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,16 @@ import {UserFilled} from '@carbon/icons-react'
 import logo from '../../assets/logo.png'
 import AppSwitcher from '../AppSwitcher/AppSwitcher';
 import Menu from '../Menu/Menu';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+  const [label, setLabel] = useState('DE');
+
+  const updateLabel = (e)=>{
+    setLabel(e.target.value)
+  }
+
   return (
     <>
     <div className='header'>
@@ -16,13 +24,16 @@ const Header = () => {
         </div>
       </div>
       <div className='rightSection'>
-      <div className="custom-select">
-        <span className="selected-language">DE</span>
-        <div className="arrow-down"></div>
-      <select className='select'>
-        <option value="de"></option>
-      </select>
-    </div>
+        <div className="custom-select">
+          <div className="select-label">{label}</div>
+          <FontAwesomeIcon icon={faAngleDown} className='select-arrow'/>
+          <select id="mySelect" onChange={updateLabel}>
+              <option value="DE">DE</option>
+              <option value="FR">FR</option>
+              <option value="IT">IT</option>
+              <option value="EN">EN</option>
+          </select>
+        </div>
         <AppSwitcher></AppSwitcher>
         <div className='user'>
           <UserFilled size={32}/>
