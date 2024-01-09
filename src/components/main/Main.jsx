ec1890928297e59d88310280edd3d63d056cc2eb
Gil <gilvicentevieira@gmail.com>
2024-01-09 21:50:21 +0000

    WIP adding main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import styles from './Main.module.css'
+import { Helicopter, IbmCloudEventNotification } from '@carbon/icons-react';
 
 const Main = () => {
   return (
     <main>
         <section className={styles.section}>
             <div className={styles.benchmark}>
-                a
+                <IbmCloudEventNotification size={32}></IbmCloudEventNotification>
+                <span>Benchmarking</span>
             </div>
             <div>
-                b
+                <img src="https://www.ivins.com/wp-content/uploads/2020/09/placeholder-1.png" className={styles.wallpaper}/>
             </div>
             <div>
                 c
