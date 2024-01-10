baa3ea425bd3936c4a340b0361fd4821d186cd69
Gil <gilvicentevieira@gmail.com>
2024-01-10 11:42:10 +0000

    Added social container to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,33 @@
+import styles from './Footer.module.css'
+import { LogoFacebook, LogoLinkedin, LogoTwitter } from '@carbon/icons-react';
+
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className={styles.container}>
+      <div className={styles['social-container']}>
+        <div className={styles['social-left']}>
+          <span>Folgen Sie uns</span>
+          <div className={styles['social-icons']}>
+            <LogoFacebook />
+            <LogoLinkedin />
+            <LogoTwitter />
+            <span>Newsletter Abo</span>
+          </div>
+        </div>
+        <div className={styles['social-right']}>
+          <span>Kontaktieren Sie uns</span>
+          <span>Kontakt</span>
+        </div>
+      </div>
+      <div className={styles['address-container']}>
+        <div className={styles['address-left']}>
+
+        </div>
+        <div className={styles['address-right']}>
+
+        </div>
+      </div>
+    </footer>
   )
 }
 
