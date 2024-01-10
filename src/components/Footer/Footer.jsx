1ba7a3af38d0d198db19a8a353c0fe0e8d06a30a
Gil <gilvicentevieira@gmail.com>
2024-01-10 14:09:06 +0000

    Concluded footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,10 +21,23 @@ const Footer = () => {
       </div>
       <div className={styles['address-container']}>
         <div className={styles['address-left']}>
-
+          <span>AGB</span>
+          <span>Datenschutzerklarung</span>
+          <span>Hilfe</span>
         </div>
         <div className={styles['address-right']}>
-
+          <div className={styles['address-name']}>
+            <span>Schweizerischer Ingenieur - und Architektenverein</span>
+            <span>Société suisse des ingénieurs et des architectes</span>
+            <span>Società svizzera degli ingegneri e degli architetti</span>
+            <span>Swiss society of engineers and architects</span>
+          </div>
+          <div className={styles['address-contact']}>
+            <span>+41 44 283 15 15</span>
+            <span>contact@sia.ch</span>
+            <span>Postfach</span>
+            <span>8027 Zurich</span>
+          </div>
         </div>
       </div>
     </footer>
